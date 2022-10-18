21cd2226adc71d1893e49d892d25d3c9fa4d38de
Adriana <adrianascosta9@gmail.com>
2022-10-17 22:16:04 -0300

    exercicio-useState

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -3,20 +3,24 @@ import {Card, PokemonName, PokemonType, EvolveButton} from './styles'
 
 
 
+
 const PokemonCard = (props) => {
 
-    const evoluirPokemon = () => {
-        console.log("Cliquei no botão de evoluir")
-    }
+    // const evoluirPokemon = () => {
+    //     console.log("Cliquei no botão de evoluir")
+    // }
     
   return (
-    <Card color={}>
-        <img src={} alt={`Pokemon`}/>
-        <PokemonName>{}</PokemonName>
-        <PokemonType>{}</PokemonType>
-        <p>{}kg</p>
+    <Card color={props.color}>
+    
+        <img src={props.image} alt={`Pokemon`}/>
+        <PokemonName>{props.name}</PokemonName>
+        <PokemonType>{props.type}</PokemonType>
+        <p>{props.weight}kg</p>
 
-        <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
+        <EvolveButton onClick={props.evoluir}>Evoluir!</EvolveButton>           
+        <EvolveButton onClick={props.voltar}>voltar!</EvolveButton> 
+        
     </Card>
   )
 }
